57e473279e7ce0724f8e21ad13d17737ba23fc34
DaionCLS <kaxa.kverashvili@gmail.com>
2024-10-30 15:11:31 +0300

    player movment

---
+++ characters/testPlayer/code/test_player.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+var speed = 300 
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down") #var которая получает векторное значение (-x, +x, -y, +y)  в зависимости от нажатой кнопки ("left", "right", "up", "down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
